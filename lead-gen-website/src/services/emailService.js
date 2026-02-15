ef7584b8db94f96e83627ad90017012b2814ae1c
prachi2608 <prachiraval2608@gmail.com>
2026-02-15 17:15:57 -0400

    Fix nodemailer method name: createTransporter -> createTransport

--- lead-gen-website/src/services/emailService.js
+++ lead-gen-website/src/services/emailService.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 // Create email transporter
 const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
